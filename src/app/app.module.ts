f74ee6ff64ab1885576009d54d7779a69a0905b7
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-16 14:31:18 +0200

    add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AboutPageComponent } from './about-page/about-page.component';
     NavbarComponent,
     HeroComponent,
     HomePageComponent,
-    AboutPageComponent
+    AboutPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent implements OnInit {
+  heroHeading1: string = 'We are your ultimate';
+  heroHeading2: string = 'Winter experience Provider!';
+  heroText: string =
+    `With hundreds of winter holiday destinations offered, we are your natural
+  choice for planning it all out`!;
+  heroBtnText: string = 'Lern more';
+  heroBtnURL: string = '#';
+
   constructor() {}
   ngOnInit(): void {}
 }
